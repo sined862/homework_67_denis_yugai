1353f64e2b38086436a3dcae7e3c81fc70013477
Yugai Denis <yugai_denis@mail.ru>
2022-11-10 15:03:48 +0600

    Подключение jquery CDN + Написание скрипта

--- task_1/script.js
+++ task_1/script.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(function(){
+    let btnOpen = $('#add-item-btn');
+    let container = $('#container');
+    btnOpen.on('click', function(){
+        container.append('<div id="notice"></div>');
+        $('#notice').append('<p>Уведомление</p>');
+        $('#notice').append('<input id="close-btn" type="button" value="Закрыть">');
+        console.log($('p'));
+        btnClose = $('#close-btn');
+        setTimeout(function(){
+            $('#notice').remove();
+        }, 5000);
+        btnClose.on('click', function(){
+            $('#notice').remove();
+        })
+    });
+})
